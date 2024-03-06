88cdf537d707477226befc732e7dc816395e2d3e
John Nkp <john.nkp1@gmail.com>
2024-03-07 01:31:04 +1100

    fix: minor fix

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   
   # resources :home do
   #   post :update_url, on: :collection
-  # end
+  # end hhhhh
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
